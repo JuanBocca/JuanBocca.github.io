5597e4e3dd5b99c67a01e9703e6d04a5239990a8
Juan <juanbocca321@gmail.com>
2020-07-26 23:06:35 -0300

    toggle

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,4 +2,27 @@ window.addEventListener('load', function (e) {
   setTimeout(function () {
     document.querySelector('#spinner-wrapper').style.display = 'none';
   }, 200);
+
+  window.addEventListener('scroll', function () {
+    var header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > ((document.documentElement.clientHeight / 2) - 100));
+  });
+
+  var toggle = document.querySelector('.hamburger');
+  var nav = document.querySelector('.nav');
+
+  var media = window.matchMedia('(min-width: 665px)');
+  media.addListener(function () {
+    if (media.matches) {
+      nav.classList.remove('mobile-nav');
+      toggle.classList.remove('is-active');
+    }
+  });
+
+  toggle.addEventListener('click', function (e) {
+    e.preventDefault();
+    this.classList.toggle('is-active');
+    nav.classList.toggle('mobile-nav');
+  });
+
 });
