7824e79821105f245357ab5b236ed3aed7ce5ac6
Juan <juanbocca321@gmail.com>
2020-07-28 14:56:36 -0300

    toggle-fix

--- assets/js/script.js
+++ assets/js/script.js
@@ -8,46 +8,29 @@ window.addEventListener('load', function (e) {
     header.classList.toggle('sticky', window.scrollY > ((document.documentElement.clientHeight / 2) - 100));
   });
 
+  var mediaS = window.matchMedia('(max-width: 664px)');
+
   var toggle = document.querySelector('.hamburger');
   var nav = document.querySelector('.nav');
-
-  var mediaS = window.matchMedia('(max-width: 664px)');
-  var mediaB = window.matchMedia('(min-width: 665px)');
   var li = document.querySelector('ul');
 
-  if (mediaS.matches) {
-    li.addEventListener('click', function (e) {
-      toggle.classList.toggle('is-active');
-      nav.classList.toggle('mobile-nav');
-    });
-  }
-
-  if (mediaB.matches) {
-    li.removeEventListener('click', function () {
-      toggle.classList.toggle('is-active');
-      nav.classList.toggle('mobile-nav');
-    })
-    nav.classList.remove('mobile-nav');
-    toggle.classList.remove('is-active');
-  }
-
-  window.addEventListener('resize', function (e) {
-    if (mediaB.matches) {
-      console.log('grande');
-      li.removeEventListener('click', function () {
-        e.preventDefault();
-        toggle.classList.toggle('is-active');
-        nav.classList.toggle('mobile-nav');
-      })
+  function mediaResponse(mediaS) {
+    if (mediaS.matches) {
+      li.addEventListener('click', hideShowToggle);
+    } else {
+      li.removeEventListener('click', hideShowToggle);
       nav.classList.remove('mobile-nav');
       toggle.classList.remove('is-active');
-    } else {
-      li.addEventListener('click', function (e) {
-        toggle.classList.toggle('is-active');
-        nav.classList.toggle('mobile-nav');
-      });
     }
-  });
+  }
+
+  function hideShowToggle() {
+    toggle.classList.toggle('is-active');
+    nav.classList.toggle('mobile-nav');
+  }
+
+  mediaResponse(mediaS);
+  mediaS.addListener(mediaResponse);
 
   toggle.addEventListener('click', function (e) {
     e.preventDefault();
